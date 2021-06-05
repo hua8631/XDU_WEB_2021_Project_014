b1719be519acf9b66f329652a681a118151c2ae9
hua8631 <71376460+hua8631@users.noreply.github.com>
2021-06-05 12:31:28 +0800

    编写view视图 商品目录和详情

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'simpleui',
-    'polls.apps.PollsConfig',
+    'shop.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/shop/apps.py
+++ mysite/shop/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
-    name = 'polls'
+    name = 'shop'
+
