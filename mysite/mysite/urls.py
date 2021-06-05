5ba11000fc0bef4fcd648e818c741b9198eb409b
hua8631 <71376460+hua8631@users.noreply.github.com>
2021-06-05 11:04:12 +0800

    编写view视图 商品目录和详情

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,8 @@ from django.urls import include, path
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', include('shop.urls', namespace='shop'))
+
+
+
 ]
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,5 @@
+from typing import Any, Tuple
+
 from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
@@ -124,9 +126,34 @@ class User(AbstractUser):
 
 
 
+# 功能主体models  Category and Product
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,db_index=True)
+    slug = models.SlugField(max_length=200,unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
 
 
-
-
-booking
-
+    def __str__(self):
+        return self.name
